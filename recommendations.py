14111565028a006c443523394bbd24f61ec33285
Luis <rooster.wally@gmail.com>
2019-03-14 13:15:28 -0600

    Add files via upload

---
+++ recommendations.py
@@ -0,0 +1,51 @@
+import json
+import os
+
+totalRecommendations = 0
+totalAccepts = 0
+totalClicks = 0
+totalAddsToCart = 0
+
+csv_file = "data/outcome/metrics.csv"
+csv = open(csv_file, "a")
+ 
+#open the file
+with open('data/productTrends031419AM.json') as f:
+  data = json.load(f)
+
+#File headers
+if os.path.getsize(csv_file) == 0:
+    csv.write("Timestamp,Metric,Count\n")
+  
+#Read all recommendations
+for day in data['productTrends']['ecom']['day1']['cntRecommendations']:
+    totalRecommendations += day["v"]
+    row = str(day["n"]) + ",Recommendations," + str(day["v"]) + "\n"
+    csv.write(row)
+print (str(totalRecommendations) + " recommendations")  
+
+#Read all accepts  
+for day in data['productTrends']['ecom']['day1']['cntAccepts']:
+    totalAccepts += day["v"]
+    row = str(day["n"]) + ",Accepts," + str(day["v"]) + "\n"
+    csv.write(row)
+print (str(totalAccepts) + " accepts")
+
+#Read all clicks  
+for day in data['productTrends']['ecom']['day1']['cntClicks']:
+    totalClicks += day["v"]
+    row = str(day["n"]) + ",Clicks," + str(day["v"]) + "\n"
+    csv.write(row)
+print (str(totalClicks) + " clicks")
+
+#Read all clicks  
+for day in data['productTrends']['ecom']['day1']['cntAddsToCart']:
+    totalAddsToCart += day["v"]
+    row = str(day["n"]) + ",Adds," + str(day["v"]) + "\n"
+    csv.write(row)
+print (str(totalAddsToCart) + " Adds to cart")
+
+ 
+#reading file
+csv.close()
+
