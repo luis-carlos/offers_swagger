93794b618e98738e8815c6af617d1d357478488b
Luis Carlos <luis.c.garcia@oracle.com>
2019-03-14 19:01:16 -0600

    Fixing date format

--- recommendations.py
+++ recommendations.py
@@ -1,5 +1,6 @@
 import json
 import os
+import csv
 
 totalRecommendations = 0
 totalAccepts = 0
@@ -20,28 +21,32 @@ if os.path.getsize(csv_file) == 0:
 #Read all recommendations
 for day in data['productTrends']['ecom']['day1']['cntRecommendations']:
     totalRecommendations += day["v"]
-    row = str(day["n"]) + ",Recommendations," + str(day["v"]) + "\n"
+    #row = str(day["n"]) + ",Recommendations," + str(day["v"]) + "\n"
+    row =  str(day["n"])[5:7] + "/" + str(day["n"])[8:10] + "/" + str(day["n"])[:4] + str(day["n"])[10:] + ",Recommendations," + str(day["v"]) + "\n"
     csv.write(row)
 print (str(totalRecommendations) + " recommendations")  
 
 #Read all accepts  
 for day in data['productTrends']['ecom']['day1']['cntAccepts']:
     totalAccepts += day["v"]
-    row = str(day["n"]) + ",Accepts," + str(day["v"]) + "\n"
+    #row = str(day["n"]) + ",Accepts," + str(day["v"]) + "\n"
+    row =  str(day["n"])[5:7] + "/" + str(day["n"])[8:10] + "/" + str(day["n"])[:4] + str(day["n"])[10:] + ",Accepts," + str(day["v"]) + "\n"
     csv.write(row)
 print (str(totalAccepts) + " accepts")
 
 #Read all clicks  
 for day in data['productTrends']['ecom']['day1']['cntClicks']:
     totalClicks += day["v"]
-    row = str(day["n"]) + ",Clicks," + str(day["v"]) + "\n"
+    #row = str(day["n"]) + ",Clicks," + str(day["v"]) + "\n"
+    row =  str(day["n"])[5:7] + "/" + str(day["n"])[8:10] + "/" + str(day["n"])[:4] + str(day["n"])[10:] + ",Clicks," + str(day["v"]) + "\n"
     csv.write(row)
 print (str(totalClicks) + " clicks")
 
 #Read all clicks  
 for day in data['productTrends']['ecom']['day1']['cntAddsToCart']:
     totalAddsToCart += day["v"]
-    row = str(day["n"]) + ",Adds," + str(day["v"]) + "\n"
+    #row = str(day["n"]) + ",Adds," + str(day["v"]) + "\n"
+    row =  str(day["n"])[5:7] + "/" + str(day["n"])[8:10] + "/" + str(day["n"])[:4] + str(day["n"])[10:] + ",Adds," + str(day["v"]) + "\n"
     csv.write(row)
 print (str(totalAddsToCart) + " Adds to cart")
 
