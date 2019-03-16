3772f02a4af9224664b5edc6daa9072affc95d72
Luis Carlos <luis.c.garcia@oracle.com>
2019-03-16 10:12:21 -0600

    Clearing unnecessary commented out lines and defining output file

--- recommendations.py
+++ recommendations.py
@@ -7,11 +7,10 @@ totalAccepts = 0
 totalClicks = 0
 totalAddsToCart = 0
 
-csv_file = "data/outcome/metrics.csv"
+csv_file = "data/outcome/metrics-dedup.csv"
 csv = open(csv_file, "a")
  
 #open the file
-#with open('data/productTrends031519AM.json') as f:
 with open(sys.argv[1]) as f:
   data = json.load(f)
 
@@ -22,7 +21,6 @@ if os.path.getsize(csv_file) == 0:
 #Read all recommendations
 for day in data['productTrends']['ecom']['day1']['cntRecommendations']:
     totalRecommendations += day["v"]
-    #row = str(day["n"]) + ",Recommendations," + str(day["v"]) + "\n"
     row =  str(day["n"])[5:7] + "/" + str(day["n"])[8:10] + "/" + str(day["n"])[:4] + str(day["n"])[10:] + ",Recommendations," + str(day["v"]) + "\n"
     csv.write(row)
 print (str(totalRecommendations) + " recommendations")  
@@ -30,7 +28,6 @@ print (str(totalRecommendations) + " recommendations")
 #Read all accepts  
 for day in data['productTrends']['ecom']['day1']['cntAccepts']:
     totalAccepts += day["v"]
-    #row = str(day["n"]) + ",Accepts," + str(day["v"]) + "\n"
     row =  str(day["n"])[5:7] + "/" + str(day["n"])[8:10] + "/" + str(day["n"])[:4] + str(day["n"])[10:] + ",Accepts," + str(day["v"]) + "\n"
     csv.write(row)
 print (str(totalAccepts) + " accepts")
@@ -38,7 +35,6 @@ print (str(totalAccepts) + " accepts")
 #Read all clicks  
 for day in data['productTrends']['ecom']['day1']['cntClicks']:
     totalClicks += day["v"]
-    #row = str(day["n"]) + ",Clicks," + str(day["v"]) + "\n"
     row =  str(day["n"])[5:7] + "/" + str(day["n"])[8:10] + "/" + str(day["n"])[:4] + str(day["n"])[10:] + ",Clicks," + str(day["v"]) + "\n"
     csv.write(row)
 print (str(totalClicks) + " clicks")
@@ -46,7 +42,6 @@ print (str(totalClicks) + " clicks")
 #Read all clicks  
 for day in data['productTrends']['ecom']['day1']['cntAddsToCart']:
     totalAddsToCart += day["v"]
-    #row = str(day["n"]) + ",Adds," + str(day["v"]) + "\n"
     row =  str(day["n"])[5:7] + "/" + str(day["n"])[8:10] + "/" + str(day["n"])[:4] + str(day["n"])[10:] + ",Adds," + str(day["v"]) + "\n"
     csv.write(row)
 print (str(totalAddsToCart) + " Adds to cart")
