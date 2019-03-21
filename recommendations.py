5bd5c7313a6d478d85f6af87ba1218b4a71f8ff4
Luis Carlos <luis.c.garcia@oracle.com>
2019-03-20 19:52:28 -0600

    Refactoring

--- recommendations.py
+++ recommendations.py
@@ -2,13 +2,17 @@ import json
 import os
 import sys
 
-totalRecommendations = 0
-totalAccepts = 0
-totalClicks = 0
-totalAddsToCart = 0
-
 csv_file = "data/outcome/metrics-dedup.csv"
 csv = open(csv_file, "a")
+
+def parseJSON(csv, data, metricName, metricText):
+    totalMetric = 0
+    for day in data['productTrends']['ecom']['day1'][metricName]:
+        totalMetric += day["v"]
+        timestamp = str(day["n"])[5:7] + "/" + str(day["n"])[8:10] + "/" + str(day["n"])[:4] + str(day["n"])[10:]
+        row = timestamp  + "," + metricText +  "," + str(day["v"]) + "," + timestamp + metricText + "\n"
+        csv.write(row)
+    print (str(totalMetric), metricText)
  
 #open the file
 with open(sys.argv[1]) as f:
@@ -18,43 +22,10 @@ with open(sys.argv[1]) as f:
 if os.path.getsize(csv_file) == 0:
     csv.write("Timestamp,Metric,Count\n")
   
-#Read all recommendations
-for day in data['productTrends']['ecom']['day1']['cntRecommendations']:
-    totalRecommendations += day["v"]
-    timestamp = str(day["n"])[5:7] + "/" + str(day["n"])[8:10] + "/" + str(day["n"])[:4] + str(day["n"])[10:]
-    metric = "Recommendations"
-    row = timestamp  + "," + metric +  "," + str(day["v"]) + "," + timestamp + metric + "\n"
-    csv.write(row)
-print (str(totalRecommendations) + " recommendations")  
+parseJSON(csv, data, "cntRecommendations", "Recommendations")
+parseJSON(csv, data, "cntAccepts", "Accepts")
+parseJSON(csv, data, "cntClicks", "Clicks")
+parseJSON(csv, data, "cntAddsToCart", "Adds")
 
-#Read all accepts  
-for day in data['productTrends']['ecom']['day1']['cntAccepts']:
-    totalAccepts += day["v"]
-    timestamp = str(day["n"])[5:7] + "/" + str(day["n"])[8:10] + "/" + str(day["n"])[:4] + str(day["n"])[10:]
-    metric = "Accepts"
-    row = timestamp  + "," + metric + "," + str(day["v"]) + "," + timestamp + metric + "\n"
-    csv.write(row)
-print (str(totalAccepts) + " accepts")
-
-#Read all clicks  
-for day in data['productTrends']['ecom']['day1']['cntClicks']:
-    totalClicks += day["v"]
-    timestamp = str(day["n"])[5:7] + "/" + str(day["n"])[8:10] + "/" + str(day["n"])[:4] + str(day["n"])[10:]
-    metric = "Clicks"
-    row =  timestamp + "," + metric + "," + str(day["v"]) + "," + timestamp + metric + "\n"
-    csv.write(row)
-print (str(totalClicks) + " clicks")
-
-#Read all clicks  
-for day in data['productTrends']['ecom']['day1']['cntAddsToCart']:
-    totalAddsToCart += day["v"]
-    timestamp = str(day["n"])[5:7] + "/" + str(day["n"])[8:10] + "/" + str(day["n"])[:4] + str(day["n"])[10:]
-    metric = "Adds"
-    row =  timestamp + "," + metric + "," + str(day["v"]) + "," + timestamp + metric + "\n"
-    csv.write(row)
-print (str(totalAddsToCart) + " Adds to cart")
-
- 
-#reading file
 csv.close()
 
