6465f4fe1fc59db9b7ecd83d9eba058d45174c1d
Luis Carlos <luis.c.garcia@oracle.com>
2019-04-03 16:33:17 -0600

    WIP for RESTful API

--- api_sample.py
+++ api_sample.py
@@ -1,7 +1,7 @@
 import requests
 
 def get_data():
-    apex_api_url = 'https://apex.oraclecorp.com/pls/apex/lcg-demo/hr/employees/'
+    apex_api_url = 'https://apex.oraclecorp.com/pls/apex/lcg-demo/hr/employees/7839'
     request_data = requests.get(apex_api_url)
     return request_data.json()
 
